21558f87353e831281207cd910692883e6dd24f1
Lucas Zapico <zapicolucas@gmail.com>
2021-03-04 22:12:59 -0700

    Add Chakra ui provider to app root This exposes Chakra styled components throughout the application.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import dotenv from 'dotenv'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+
+dotenv.config()
+
+const theme = extendTheme({
+  colors: {},
+})
+
+// const theme = extendTheme({ styles })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
