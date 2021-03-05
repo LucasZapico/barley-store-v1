2e3a42b328d80bb20d86e1beca5b4d641a7c1488
Lucas Zapico <zapicolucas@gmail.com>
2021-03-04 21:57:44 -0700

    Add eslint and prettier Leveraging predefined best practices from eslint and prettier enhances codes quality.

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'only-warn'],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+  },
+}
